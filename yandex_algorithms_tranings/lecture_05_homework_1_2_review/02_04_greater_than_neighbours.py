7342efe34a3caaa03a86194022b853fbf639d9ce
Igor Barbashov <bis200705@gmail.com>
2022-09-18 13:10:20 +0300

    yandex trainings - lecture 05 - homework - 02_04_greater_than_neighbours

---
+++ yandex_algorithms_tranings/lecture_05_homework_1_2_review/02_04_greater_than_neighbours.py
@@ -0,0 +1,9 @@
+def countNumbers(l):
+    count = 0
+    for i in range(1, len(l) - 1):
+        if l[i] > l[i - 1] and l[i] > l[i + 1]:
+            count += 1
+    return count
+
+l = list(map(int, input().split(' ')))
+print(countNumbers(l))
